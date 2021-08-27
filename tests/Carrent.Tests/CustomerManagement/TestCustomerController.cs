1e841feacc4b7cd41f8644556ba9526d66cfd646
Ricardo Coelho <devel@rmorgado.ch>
2021-08-27 20:45:56 +0200

    tests/CustomerController: add tests

--- tests/Carrent.Tests/CustomerManagement/TestCustomerController.cs
+++ tests/Carrent.Tests/CustomerManagement/TestCustomerController.cs
@@ -20,23 +20,57 @@ namespace CarRent.Test.CustomerManagement
 
         private readonly Mock<IRepository<Customer, Guid>> _repository;
 
+        private readonly Customer _customerJoe = new()
+        {
+            Id = Guid.NewGuid(),
+            Firstname = "Joe",
+            Lastname = "Doe",
+            Street = "Fakestreet 1",
+            Country = "CH",
+            Town = "St. Gallen",
+            Zip = "9000"
+        };
+        private readonly Customer _customerThomas = new()
+        {
+            Id = Guid.NewGuid(),
+            Firstname = "Thomas",
+            Lastname = "Schmit",
+            Street = "Fakestreet 1",
+            Country = "CH",
+            Town = "St. Gallen",
+            Zip = "9000"
+        };
+        private readonly Customer _customerHans= new()
+        {
+            Id = Guid.NewGuid(),
+            Firstname = "Hans",
+            Lastname = "Mueller",
+            Street = "Fakestreet 1",
+            Country = "CH",
+            Town = "St. Gallen",
+            Zip = "9000"
+        };
 
-        private readonly List<Customer> _entities;
+        private readonly List<Customer> _customers;
         public TestCustomerController()
         {
 
-
-            _entities = new List<Customer>()
+            var carClasses = new List<Customer>()
+            {
+                _customerJoe ,
+                _customerThomas,
+                _customerHans
+            };
+            _customers = new List<Customer>()
             {
                 new Customer()
                 {
-                    Id = Guid.NewGuid(),
-                    Firstname = "asd",
-                    Lastname = "asdf",
-                    Street = "asdf",
-                    Country = "Switzerland",
-                    Town = "Hansel",
-                    Zip = "90123"
+                    Firstname = "Hans",
+                    Lastname = "Mueller",
+                    Street = "Fakestreet 1",
+                    Country = "CH",
+                    Town = "St. Gallen",
+                    Zip = "9000"
                 }
             };
             _mapper = new Mapper(new MapperConfiguration(conf =>
@@ -46,28 +80,19 @@ namespace CarRent.Test.CustomerManagement
 
             _repository = new Mock<IRepository<Customer, Guid>>();
 
-            _repository.Setup(x => x.GetAll()).Returns(_entities);
+            _repository.Setup(x => x.GetAll()).Returns(_customers);
             _repository.Setup(x => x.Insert(It.IsAny<Customer>()));
 
             _service = new CustomerService(_repository.Object);
         }
 
-        //[Fact]
-        //public void TestGetAll()
-        //{
-        //    var controller = new CustomerController(_service, _mapper);
-
-        //    var result = controller.Get();
-
-        //    result.Should().NotBeEmpty().And.BeEquivalentTo(_entities, o => o.ExcludingMissingMembers());
-        //}
 
         [Fact]
-        public void TestAdd()
+        public void CustomerController_Add_VerifyItemsIsAdded()
         {
+            // arrange
             var controller = new CustomerController(_service, _mapper);
-
-            var entityToAdd = new CustomerRequestCreateDto()
+            var dto = new CustomerRequestCreateDto()
             {
                 Firstname = "Ricardo",
                 Lastname = "Coelho",
@@ -77,9 +102,111 @@ namespace CarRent.Test.CustomerManagement
                 Town = "St. Gallen"
             };
 
-            controller.Post(entityToAdd);
+            //act
+            controller.Post(dto);
 
+            //assert
             _repository.Verify(x => x.Insert(It.IsAny<Customer>()));
+        }
+
+        [Fact]
+        public void CustomerController_Edit_VerifyItemsIsUpdated()
+        {
+            //arrange
+            var dto = new CustomerRequestEditDto()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "Ricardo",
+                Lastname = "Coelho",
+                Street = "Gaiserwaldstrasse 6",
+                Zip = "9015",
+                Country = "CH",
+                Town = "St. Gallen"
+            };
+            var controller = new CustomerController(_service, _mapper);
+
+            //act
+            controller.Put(dto.Id, dto);
+
+            //assert
+            _repository.Verify(x => x.Update(It.IsAny<Customer>()));
+        }
+
+        [Fact]
+        public void CustomerController_Delete_VerifyServiceIfWasCalled()
+        {
+            //arrange
+            var model = _customerJoe;
+
+            //set up the repository’s Delete call
+            _repository.Setup(r => r.Remove(It.IsAny<Customer>()));
+
+            //act
+            _service.Delete(model);
+
+            //assert
+            _repository.Verify(r => r.Remove(model));
+        }
+
+        [Fact]
+        public void CustomerController_DeleteById_VerifyIfServiceWasCalled()
+        {
+            //arrange
+            var model = _customerJoe;
+
+            //set up the repository’s Delete call
+            _repository.Setup(r => r.Remove(model.Id));
+
+            //act
+            var controller = new CustomerController(_service, _mapper);
+
+            //act
+            controller.Delete(model.Id);
+
+            //assert
+            _repository.Verify(r => r.Remove(model.Id));
+        }
+
+        [Fact]
+        public void CustomerController_FindById_ReturnsCustomer()
+        {
+            //arrange
+            var dto = new CustomerRequestEditDto()
+            {
+                Id = Guid.NewGuid(),
+                Firstname = "Ricardo",
+                Lastname = "Coelho",
+                Street = "Gaiserwaldstrasse 6",
+                Zip = "9015",
+                Country = "CH",
+                Town = "St. Gallen"
+            };
+
+            _repository.Setup(x => x.FindById(dto.Id));
+
+            var controller = new CustomerController(_service, _mapper);
+
+            //act
+            var result = controller.Get(dto.Id);
+
+            //assert
+            _repository.Verify(r => r.FindById(dto.Id));
+        }
+
+
+        [Fact]
+        public void CustomerController_GetAll_ReturnsList()
+        {
+            //arrange
+            _repository.Setup(x => x.GetAll()).Returns(_customers);
+            var controller = new CustomerController(_service, _mapper);
+
+            //act
+            var actionResult = controller.Get();
+            var result = actionResult;
+
+            //assert
+            Assert.Equal(_customers.Count, result.Count);
         }
     }
 }
